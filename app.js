554d0734b9ec450c47f1105e414a1939fa0045ef
sahvsergio <sergioandresherrera@gmail.com>
2025-02-28 22:50:36 -0500

    add root route to app

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
 
+//create the app
+
 const app=express();
+
+//add routes 
+
+app.get('/', (request, response)=>{
+    //response.send , sends a 
+    response.send('I love Treehouse');
+
+
+})
+
+//open port for the app
 
 app.listen(3000);
