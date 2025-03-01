ad0eeaa26ca120e41feb49b0f3007218fcf20e2e
sahvsergio <sergioandresherrera@gmail.com>
2025-02-28 23:24:48 -0500

    update the variables of the routes to req and res

--- app.js
+++ app.js
@@ -6,12 +6,26 @@ const app=express();
 
 //add routes 
 
-app.get('/', (request, response)=>{
-    //response.send , sends a 
-    response.send('I love Treehouse');
+app.get('/', (req, res)=>{
+    //res.send , sends a 
+    res.send('I love Treehouse');
 
 
 })
+
+app.get("/about", (req, res) => {
+  //res.send , sends a
+  res.send("I love Treehouse");
+});
+
+app.get("/project:id", (req, res) => {
+  //res.send , sends a
+  res.send("I love Treehouse");
+});
+app.get("/projects:id", (req, res) => {
+  //res.send , sends a
+  res.send("I love Treehouse");
+});
 
 //open port for the app
 
