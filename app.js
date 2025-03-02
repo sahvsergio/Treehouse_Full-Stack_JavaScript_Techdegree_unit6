1a582dde122988972b2d4e0f478a0b4206071432
sahvsergio <sergioandresherrera@gmail.com>
2025-03-01 20:58:27 -0500

    add pug template to the index

--- app.js
+++ app.js
@@ -4,29 +4,35 @@ const express = require("express");
 
 const app=express();
 
+//set template engine to pug
+
+app.set('view engine', 'pug');
+
 //add routes 
 
 app.get('/', (req, res)=>{
-    //res.send , sends a 
-    res.send('I love Treehouse');
+    //res.render , renders a 
+    res.render('index');
 
 
 })
 
 app.get("/about", (req, res) => {
-  //res.send , sends a
-  res.send("I love Treehouse");
+  //res.render , renders a
+  res.render("I love Treehouse");
 });
 
 app.get("/project:id", (req, res) => {
-  //res.send , sends a
-  res.send("I love Treehouse");
+  //res.render , renders a
+  res.render("I love Treehouse");
 });
 app.get("/projects:id", (req, res) => {
-  //res.send , sends a
-  res.send("I love Treehouse");
+  //res.render , renders a
+  res.render("I love Treehouse");
 });
 
 //open port for the app
 
-app.listen(3000);
+app.listen(3000,()=>{
+  console.log('The application is running on localhost:3000');
+});
