f28ddeaa2c87b988d5c2c2fb262c24a4ad82ede3
sahvsergio <sergioandresherrera@gmail.com>
2025-03-03 22:12:16 -0500

    add css to the templates

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
+app.use('/static', express.static('public'));
 
 //set template engine to pug
 
