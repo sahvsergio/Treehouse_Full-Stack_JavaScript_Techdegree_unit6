0bee857eb130d1618ab587ae341ac63f9a1e1501
sahvsergio <sergioandresherrera@gmail.com>
2025-03-07 08:45:41 -0500

    check error handlers

--- app.js
+++ app.js
@@ -23,37 +23,46 @@ app.set('view engine', 'pug');
 const routes= require('./routes');
 //open port for the app
 
+
+
+
+
+
+
 app.use(routes);
 
-app.use((req, res, next)=>{
-  const err= new Error('Ohh something happened ');
-  err.status=500;
-  next(err);
-});
 
-
-app.use((err, req, res, nex) => {
-  res.locals.error = err;
-  res.status(err.status);
-  console.log(err.message);
-  res.render("error", err);
-});
 
 
 app.use((req, res, next)=>{
   const err= new Error('Page Not Found');
   err.status=404;
   next(err);
-
 });
+  
 
 app.use((err,req, res, nex)=>{
   res.locals.error=err;
   res.status(err.status);
-  console.log('The page you are looking for was not found');
-  res.render('page-not-found', err);
+  if (err.status===404){
+    console.log("The page you are looking for was not found");
+     res.render("page-not-found", err);
+  }
+    
+ 
 
-})
+  else{
+     console.log(err.message);
+     res.render("error", err);
+  }
+
+     
+
+});
+
+
+
+
 
   
  
@@ -63,6 +72,6 @@ app.use((err,req, res, nex)=>{
 
 
 
-app.listen(5000,()=>{
-  console.log('The application is running on localhost:5000');
+app.listen(9000,()=>{
+  console.log('The application is running on localhost:9000');
 });
